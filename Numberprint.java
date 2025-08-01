5ad25c9ca1b6f365d4c2534ea7a04377d2dd22e6
Chandrakant Mali <161549812+chandrakant-mali85@users.noreply.github.com>
2025-08-01 17:46:45 +0530

    done

---
+++ Numberprint.java
@@ -0,0 +1,19 @@
+
+
+public class Numberprint{
+
+public static void main(String args[]){
+
+int number=8673;
+
+int first=number/1000;
+int last=number%10;
+int sum=first+last;
+
+System.out.println("First:"+first);
+
+System.out.println("last :"+last);
+System.out.println("sum:"+sum);
+}
+
+}
